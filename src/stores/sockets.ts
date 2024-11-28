d875bfc2c4f71cbb960cf04419a1aed770659df0
ryhuxdev <ryhuxdev@gmail.com>
2024-11-29 00:00:51 +0300

    добавил отправку сообщений и улучшил логирование

--- src/stores/sockets.ts
+++ src/stores/sockets.ts
@@ -8,28 +8,38 @@ export const useSocketsStore = defineStore("sockets", () => {
     const connection = ref<WebSocket | null>(null);
 
     const connect = (token: string) => {
-        const proto = location.protocol === 'https:' ? 'wss' : 'ws';
+        const proto = location.protocol === "https:" ? "wss" : "ws";
         connection.value = new WebSocket(`${proto}://${APP_BACKEND_URL}/ws?token=${token}`);
 
         connection.value.onopen = () => {
-            logger.log("Подключено");
+            logger.info("Подключено");
         };
 
         connection.value.onclose = () => {
-            logger.log("Отключено");
+            logger.info("Отключено");
             connection.value = null;
         };
-    }
+    };
 
     const close = () => {
-        logger.log("Отключено");
+        logger.info("Отключено");
         connection.value?.close();
         connection.value = null;
-    }
+    };
+
+    const send = (message: any) => {
+        if (!connection.value) {
+            logger.error("Данные не отправлены: нет подключения");
+            return;
+        }
+        connection.value.send(JSON.stringify(message));
+        logger.info("Данные отправлены");
+    };
 
     return {
         connection,
         connect,
-        close
-    }
+        close,
+        send,
+    };
 })
