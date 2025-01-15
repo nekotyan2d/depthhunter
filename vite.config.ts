a4df13c1e6950605907011b64359051725bfbf35
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2025-01-15 22:33:37 +0300

    chore: threejs и vue выделены в отдельные чанки

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,22 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-		vue()
-	],
-	css: {
-		preprocessorOptions: {
-			scss: {
-				api: "modern-compiler"
-			}
-		}
-	}
-})
+    plugins: [vue()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern-compiler",
+            },
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vue: ["vue", "vue-router", "pinia"],
+                    three: ["three"],
+                },
+            },
+        },
+    },
+});
