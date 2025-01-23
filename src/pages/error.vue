a31a13e9f4151cf7e147226f77ec995ceb13af47
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2025-01-23 20:20:31 +0300

    feat: добавлена кнопка перезагрузки страницы на экране с ошибкой

--- src/pages/error.vue
+++ src/pages/error.vue
@@ -2,10 +2,16 @@
     <div class="error">
         <Image src="/img/cross.webp" alt="Error occurred" width="150" height="150" />
         <p>{{ error.message }}</p>
+        <Button @click="refresh">Перезагрузить</Button>
     </div>
 </template>
 <script lang="ts" setup>
 import Image from '../components/Image.vue';
+import Button from '../components/Button.vue';
+
+function refresh(){
+    window.location.reload();
+}
 
 defineProps<{
     error: {
